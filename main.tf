205bb11cc2502ac30e0758fd6ef3ad5bcdca7ffa
Amol Ghadge <64397760+Amolghadge@users.noreply.github.com>
2022-04-11 20:25:11 +0530

    vminstance
    Test

---
+++ main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_instance" "vminstance" {
+  name         = "vminstance"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-b"
+
+  tags = ["db"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {}
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    sshKeys = "ubuntu:${file(var.ssh_public_key_filepath)}"
+  }
+
+}
